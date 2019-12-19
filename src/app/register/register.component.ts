28d25291d6fa4f1fd24f80efd065743fbe8d8c2a
barron419 <wreall113@gmail.com>
2019-12-18 21:29:10 -0700

    Registration Page Added

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,40 @@
+import { FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+// import { User } from './../backend/models/user.js';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.sass']
+})
+export class RegisterComponent implements OnInit {
+  model: any = {};
+  registerForm = new FormGroup ({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    fname: new FormControl(''),
+    lname: new FormControl(''),
+    email: new FormControl('')
+  });
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  async register(){
+    //   const user = new User({       // course is an object of type Course (lowercase)
+    //   username: this.registerForm.get('username').value,
+    //   password: this.registerForm.get('password').value,
+    //   fname: this.registerForm.get('fname').value,
+    //   lname: this.registerForm.get('lname').value,
+    //   email: this.registerForm.get('email').value
+    //  });
+
+    //   const result = await user.save();
+
+    //   console.log(result);
+
+
+  }
+}
